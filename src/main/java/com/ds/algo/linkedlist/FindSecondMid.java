16649a153b2d8536b04a8dee05563e0f738c23ae
krishnaprasadpadala <krishnaprasad.padal@outlook.com>
2025-02-21 18:27:32 +0530

    find second mid in Linked List

---
+++ src/main/java/com/ds/algo/linkedlist/FindSecondMid.java
@@ -0,0 +1,28 @@
+package com.ds.algo.linkedlist;
+
+public class FindSecondMid {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node two = head.next = new Node(2);
+        Node three = two.next = new Node(3);
+        Node four = three.next = new Node(4);
+        Node five = four.next = new Node(5);
+        Node six = five.next = new Node(6);
+        System.out.println(secondMid(head).data);
+    }
+
+    private static Node secondMid(Node head) {
+        if(head.next == null) {
+            return head;
+        }
+
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
