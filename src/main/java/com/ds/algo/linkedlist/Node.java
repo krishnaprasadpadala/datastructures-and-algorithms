f86677d3c0e96eea473a4f0af1b6d6f2c9693c78
krishnaprasadpadala <krishnaprasad.padal@outlook.com>
2025-02-21 18:16:30 +0530

    find first mid

--- src/main/java/com/ds/algo/linkedlist/Node.java
+++ src/main/java/com/ds/algo/linkedlist/Node.java
@@ -5,7 +5,16 @@ public class Node {
     public Node next;
     public int data;
     
+    public Node prev;
+    public int key;
+    
     public Node(int data) {
         this.data = data;
     }
+    
+    public Node(int key, int data) {
+        this.key = key;
+        this.data = data;
+    }
+    
 }
