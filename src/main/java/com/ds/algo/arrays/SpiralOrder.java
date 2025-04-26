9a10e0695dc51adfb3dcc86db05da6ec9e5447d3
krishnaprasadpadala <krishnaprasad.padal@outlook.com>
2025-04-26 18:11:04 +0530

    Print array in Spiral order

---
+++ src/main/java/com/ds/algo/arrays/SpiralOrder.java
@@ -0,0 +1,57 @@
+package com.ds.algo.arrays;
+
+import java.util.*;
+
+public class SpiralOrder {
+    
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        System.out.println(spiralOrder(matrix));
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if(matrix.length == 1 && matrix[0].length == 1) {
+            result.add(matrix[0][0]);
+        }
+
+        int rowStart = 0;
+        int rowEnd = matrix.length-1;
+        int colStart = 0;
+        int colEnd = matrix[0].length-1;
+
+        while(rowEnd >= rowStart && colEnd >= colStart) {
+            for(int i=colStart; i<=colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            for(int i=rowStart; i<=rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+            
+            if(rowEnd > rowStart) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+            
+            if(colEnd > colStart) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
+        }
+        return result;
+    }
+}
